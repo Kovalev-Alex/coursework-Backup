abc9cf796182532a088bc0f39922b7fe3f5f0cf5
Alexandr Kovalev <alex_kov@inbox.ru>
2023-10-22 20:40:11 +0500

    delete user_id

--- Main.py
+++ Main.py
@@ -6,13 +6,12 @@ from Yandex_API import YAAPIClient
 
 if __name__ == '__main__':
 
-    TOKEN_VK = os.environ['APIVK']
-    TOKEN_YA = os.environ['APIYA']
-    # user_id = int(input('Введите ID пользователя (только цифры): '))
-    user_id = 90991960
-    vk_client = VKAPIClient(TOKEN_VK, user_id)
-    pprint(vk_client.get_photos())
-
-    client = YAAPIClient(TOKEN_YA)
-    # print(client.list_dir('/'))
-    print(client.upload_file('Image', 'VK_API.py'))
+    # TOKEN_VK = os.environ['APIVK']
+    # TOKEN_YA = os.environ['APIYA']
+    # # user_id = int(input('Введите ID пользователя (только цифры): '))
+    # vk_client = VKAPIClient(TOKEN_VK, user_id)
+    # pprint(vk_client.get_photos())
+    #
+    # client = YAAPIClient(TOKEN_YA)
+    # # print(client.list_dir('/'))
+    # print(client.upload_file('Image', 'VK_API.py'))
